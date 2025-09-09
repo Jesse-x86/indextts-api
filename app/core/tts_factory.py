fc5fe61a28c64f5bc56d944465486e7b357f91f0
Jesse-x86 <144566795+Jesse-x86@users.noreply.github.com>
2025-09-09 17:28:35 +0800

    upgrade codebase to v2

--- app/core/tts_factory.py
+++ app/core/tts_factory.py
@@ -1,5 +1,5 @@
 from app.config import config as app_config, AppConfig
-from app.core.tts_wrapper import TTSWrapper
+from app.core.tts_wrapper import TTSWrapper, TTSWrapperV1, TTSWrapperV2
 
 
 class TTSFactory:
@@ -8,4 +8,10 @@ class TTSFactory:
         self.config = config
 
     def get_tts(self) -> TTSWrapper:
-        return TTSWrapper(config=self.config)
+        return self.get_tts_v2()
+
+    def get_tts_v1(self):
+        return TTSWrapperV1(config=self.config)
+
+    def get_tts_v2(self):
+        return TTSWrapperV2(config=self.config)
